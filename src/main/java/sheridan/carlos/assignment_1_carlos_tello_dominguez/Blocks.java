87c01c8aa96860846a2c678926c0fd11820ccea5
Carlos Tello Dominguez <tellodom@sheridancollege.ca>
2026-01-31 18:24:46 -0500

    Added block of images to the window

---
+++ src/main/java/sheridan/carlos/assignment_1_carlos_tello_dominguez/Blocks.java
@@ -0,0 +1,56 @@
+package sheridan.carlos.assignment_1_carlos_tello_dominguez;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Blocks implements Serializable {
+
+    private final Random random = new Random();
+    private final List<Character> letters;
+
+    public Blocks() {
+        this.letters = new ArrayList<>(32);
+        for (char letter = 'A'; letter <= 'Z'; letter++){
+            this.letters.add(letter);
+        }
+    }
+
+    public synchronized List<Character> getLetters() {
+        return List.copyOf(letters);
+    }
+
+    public int getCount() {
+        return this.letters.size();
+    }
+
+    public synchronized void sortBlocks() {
+        this.letters.sort(null);
+    }
+
+    public synchronized void shuffleBlocks() {
+        for (int i = 0; i < this.letters.size(); i++) {
+            int randomIndex = random.nextInt(this.letters.size());
+            char temp = this.letters.get(i);
+            this.letters.set(i, this.letters.get(randomIndex));
+            this.letters.set(randomIndex, temp);
+        }
+    }
+
+    public synchronized void reverseBlocks() {
+        for (int i = 0; i < this.letters.size() / 2; i++) {
+            char temp = this.letters.get(i);
+            this.letters.set(i, this.letters.get(this.letters.size() - 1 - i));
+            this.letters.set(this.letters.size() - 1 - i, temp);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Blocks{" +
+                "letters=" + letters +
+                '}';
+    }
+
+}
--- src/main/java/sheridan/carlos/assignment_1_carlos_tello_dominguez/controller/UserController.java
+++ src/main/java/sheridan/carlos/assignment_1_carlos_tello_dominguez/controller/UserController.java
@@ -1,14 +1,61 @@
 //package sheridan.carlos.controller;
 package sheridan.carlos.assignment_1_carlos_tello_dominguez.controller;
 
+import sheridan.carlos.assignment_1_carlos_tello_dominguez.Blocks;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@Slf4j
 public class UserController {
 
     @GetMapping("/")
     public String showInput() {
-        return "input"; // loads input.html
+        return "redirect:/input"; // loads input.html
+    }
+
+    @GetMapping("/input") public String displayBlocks(
+            HttpSession session,
+            Model model) {
+        log.trace("Display all images");
+        Blocks blocks = getBlocks(session);
+        model.addAttribute("letters", blocks.getLetters());
+        return "input";
+    }
+
+    private Blocks getBlocks(HttpSession session) {
+        log.trace("getBlocks() is called");
+        Blocks blocks = (Blocks) session.getAttribute("blocks");
+        if(blocks == null){
+            log.trace("Blocks object is not found in the Session; making new Blocks object");
+            blocks = new Blocks();
+            session.setAttribute("blocks", blocks);
+        }else{
+            log.trace("Previous Blocks object is found in the Session");
+        }
+        return blocks;
+    }
+
+    @PostMapping("/process")
+    public String processBlocks(
+            @RequestParam String todo,
+            HttpSession session
+    ) {
+        log.trace("processBlocks() is called");
+        Blocks blocks = getBlocks(session);
+        switch (todo) {
+            case "reverse" -> blocks.reverseBlocks();
+            case "sort" -> blocks.sortBlocks();
+            case "shuffle" -> blocks.shuffleBlocks();
+        }
+        return "redirect:/input";
     }
 }
+
+
